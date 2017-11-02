56a6be5b5c777ee8c4ed73f20ae61bdd9cf5b114
agata <byszkiewicza@gmail.com>
2017-11-02 15:01:21 +0100

    przyklad pierwszy: klasa bazowa punkt

---
+++ dziedziczenie_przyklad1/src/Punkt2D.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Agata on 02.11.2017.
+ */
+public class Punkt2D {
+        int x;
+        int y;
+
+        public Punkt2D() {
+            x=0;
+            y=0;
+        }
+
+        public Punkt2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+}
